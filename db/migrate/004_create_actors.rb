323e3722ad88c79f4b91fe6b2f48bf87a3b14e07
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-04 00:45:50 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migrations[5.1]
   def change
-    create_table :actors do |t| 
+    create_table :actors do |t|
       t.string :first_name
       t.string :last_name
     end
-  end 
+  end
 end
